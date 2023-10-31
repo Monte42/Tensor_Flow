b52a940fc6fac41c07439b5d014a3e9d6886ec29
Monte DuMond <gdumond42@gmail.com>
2023-10-31 18:46:10 -0400

    Gradients Return None

--- learn_off_docs/Basics/auto_differentiation2.py
+++ learn_off_docs/Basics/auto_differentiation2.py
@@ -73,3 +73,67 @@ print(dv1)
 # The gradient with respect to x is always None
 dx = tape.gradient(result,x)
 print(dx)
+
+print('\nWorking\n')
+# ====================================
+# Cases Where A Gradient Returns None
+# ====================================
+# Here are 5 examples of what may cause a gradient to return None
+
+# Target And Source Not Connected
+# -------------------------------
+# When a target is not connected to the source, here z is tied with y, not x
+x = tf.Variable(2.0)
+y = tf.Variable(3.)
+
+with tf.GradientTape() as tape:
+    z = y**2
+print("Not connected ",tape.gradient(z,x))
+
+# Replace a Variable With a Tensor
+# ---------------------------------
+# One common error is to inadvertently replace a tf.Variable with a tf.Tensor, 
+for epoch in range(2):
+    with tf.GradientTape() as tape:
+        y = x+1
+    print(f"Epoch: {epoch} - ",type(x).__name__, ":", tape.gradient(y, x))
+    x = x + 1 # this converts x from a variable to a tensor
+            # Should have used x.assign_add(1)
+
+# Did Calculations Outside of TensorFlow
+# ---------------------------------------
+# The tape can't record the gradient path if the calculation exits TensorFlow
+x = tf.Variable([[1.,2.],[3.,4.]], dtype=tf.float32)
+
+with tf.GradientTape() as tape:
+    x2 = x**2
+    # This calculation is made with NumPy
+    y = np.mean(x2, axis=0) # returns Numpy array
+    y = tf.reduce_mean(y, axis=0) # Converts to Tensor / Not watched
+print("Calculations outside TensorFlow ",tape.gradient(y,x))
+
+# Took gradients through an integer or string. 
+# ---------------------------------------------
+# Integers and strings are not differentiable. If a calculation 
+# path uses these data types there will be no gradient.
+x = tf.constant(10) # *** Constant Not a Variable
+
+with tf.GradientTape() as tape:
+    tape.watch(x) # *** must tell tape to watch x because its a constant
+    y = x * x
+print("Int or str ",tape.gradient(y,x), "^^^^^ Thats this Warning ^^^^^") # target(y) can be tied to source(x)"constant" thanks to tape.watch
+
+# Took Gradients Through A Stateful Object
+# ------------------------------------------
+# When you use the variable, the state is read. It's normal to calculate a gradient with respect 
+# to a variable, but the variable's state blocks gradient calculations from going farther back
+# the tape can only observe the current state, not the history that lead to it.
+x0 = tf.Variable(3.0)
+x1 = tf.Variable(0.0)
+
+with tf.GradientTape() as tape:
+    x1.assign_add(x0) # This way you are addind x0 through the variable state
+    # x1 = x1 + x0 # This way you do the operation on the tape to be recorded
+    y = x1**2
+
+print(tape.gradient(y,x0))
